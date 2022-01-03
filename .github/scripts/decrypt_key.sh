463a7013a948520404f72fdad1cfe6d304292c43
danparisd <43418833+danparisd@users.noreply.github.com>
2022-01-03 16:19:19 -0500

    test more verbose

--- .github/scripts/decrypt_key.sh
+++ .github/scripts/decrypt_key.sh
@@ -3,5 +3,5 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output $GITHUB_WORKSPACE/bttkey.jks bttkey.jks.gpg
