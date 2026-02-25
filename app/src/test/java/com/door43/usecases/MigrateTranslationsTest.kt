525e61e0b8e7d1fc8fcb10888b47445c35ea1311
Maxim <murmax82@gmail.com>
2026-02-25 16:20:19 +0400

    unit test fix

--- app/src/test/java/com/door43/usecases/MigrateTranslationsTest.kt
+++ app/src/test/java/com/door43/usecases/MigrateTranslationsTest.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.net.Uri
 import com.door43.OnProgressListener
 import com.door43.data.IDirectoryProvider
+import com.door43.translationstudio.core.TargetTranslationMigrator
 import com.door43.util.FileUtilities
 import io.mockk.MockKAnnotations
 import io.mockk.every
@@ -24,6 +25,7 @@ class MigrateTranslationsTest {
     @MockK private lateinit var context: Context
     @MockK private lateinit var directoryProvider: IDirectoryProvider
     @MockK private lateinit var importProjects: ImportProjects
+    @MockK private lateinit var targetTranslationMigrator: TargetTranslationMigrator
     @MockK private lateinit var progressListener: OnProgressListener
 
     @JvmField
@@ -56,7 +58,7 @@ class MigrateTranslationsTest {
 
         val sourceFolder: Uri = mockk()
 
-        MigrateTranslations(context, importProjects, directoryProvider)
+        MigrateTranslations(context, importProjects, directoryProvider, targetTranslationMigrator)
             .execute(sourceFolder, progressListener)
 
         verify(exactly = 2) { directoryProvider.createTempDir(any()) }
